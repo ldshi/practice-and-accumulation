84e330894e00b4a428c5b900356f30c39ce016af
Leonard Shi <leonard.shi.sg@gmail.com>
2016-05-09 16:09:26 +0800

    Longest palindrome substring, improved suffix array version.

--- algorithm/leetcode/Python_2.7.10/5th.suffix_array.version.py
+++ algorithm/leetcode/Python_2.7.10/5th.suffix_array.version.py
@@ -4,7 +4,7 @@
 
 #
 ## On test case 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabcaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'
-##   still run into exceeding time limitation problem, through the performance alread got improved a lot: about 120ms
+##   still run into Memory Limit Exceeded  problem, through the performance looks good enough: about 60ms.
 #
 class Solution(object):
   def longestPalindrome(self, s):
@@ -13,9 +13,16 @@ class Solution(object):
     :rtype: str
     """
     def get_common_prefix(s_0, s_1):
+      if s_0 in s_1:
+        return (len(s_0), s_0)
+
+      if s_1 in s_0:
+        return (len(s_1), s_1)
+
       for idx in range(min(len(s_0), len(s_1))):
         if s_0[idx] != s_1[idx]:
           return (idx, s_0[0 : idx])
+
       return (0, '')
 
     if s is None:
@@ -31,25 +38,36 @@ class Solution(object):
 
     suffix_arr = []
 
-    for idx, val in enumerate(modified_s):
-      suffix_arr.append(modified_s[idx : len(modified_s)])
+    # for idx, val in enumerate(modified_s):
+    #   suffix_arr.append(modified_s[idx : len(modified_s)])
+
+    len_of_modified_s = len(modified_s)
+    for idx in range(len_of_modified_s):
+      suffix_arr.append(modified_s[idx : len_of_modified_s])
+
+    # Will this free some memory space?
+    del modified_s, len_of_modified_s
 
     suffix_arr.sort()
 
-    found_longest_common_prefix = ''
-    found_longest_common_prefix_len = 0
+    found_longest_common_prefix = s[-1]
+    found_longest_common_prefix_len = 1
 
-    for idx in range(len(suffix_arr) - 1):
-      common_prefix_len, common_prefix = get_common_prefix(suffix_arr[idx], suffix_arr[idx + 1])
-      if common_prefix_len > found_longest_common_prefix_len:
-        found_longest_common_prefix = common_prefix
-        found_longest_common_prefix_len = common_prefix_len
+    idx = len(suffix_arr) - 1
+    while idx > 0:
+      if len(suffix_arr[idx]) >= found_longest_common_prefix_len and len(suffix_arr[idx - 1]) >= found_longest_common_prefix_len:
+        common_prefix_len, common_prefix = get_common_prefix(suffix_arr[idx], suffix_arr[idx - 1])
+        if common_prefix_len > found_longest_common_prefix_len:
+          found_longest_common_prefix = common_prefix
+          found_longest_common_prefix_len = common_prefix_len
+
+      idx -= 1
 
     return found_longest_common_prefix
 
 
 
 s = Solution()
-s.longestPalindrome('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabcaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa')
+s.longestPalindrome('ccc')
 
 
