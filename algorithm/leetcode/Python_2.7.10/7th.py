2ce2c968db18e9de35a939c9140d180b00e3bd83
Leonard Shi <leonard.shi.sg@gmail.com>
2016-05-12 17:08:52 +0800

    'Reverse Integer', hehe...

--- algorithm/leetcode/Python_2.7.10/7th.py
+++ algorithm/leetcode/Python_2.7.10/7th.py
@@ -11,7 +11,7 @@ class Solution(object):
     """
     if str(x).startswith('-'):
       res = int('-' + str(x)[1:][::-1])
-      return 0 if res < -2147483647 else res
+      return 0 if res < -2147483648 else res
     else:
       res = int(str(x)[::-1])
       return 0 if res > 2147483647 else res
